334445144c7711701676a00fd3ef126d650f98fb
coulton m <58615441+coulton32@users.noreply.github.com>
2022-04-01 19:07:16 +0000

    Added string check for word guess, must be 5 letters

--- main.cpp
+++ main.cpp
@@ -14,14 +14,16 @@ std::string getNewWord() {
     while (getline(file, line)) words.push_back(line);
     return words[rand() % words.size()];
 }
-
 std::string guessWord() {
     std::cout << "Guess the word:";
     std::string a;
     std::cin >> a;
+    if (a.length() != 5) {
+      std::cout << "5 letter words only please !\n";
+      a = guessWord();
+    }
     return a;
 }
-
 std::string checkDupe(std::string s) {
     std::string p;
     bool alphabets[26];
@@ -36,7 +38,6 @@ std::string checkDupe(std::string s) {
     }
     return p;
 }
-
 int main() {
     std::cout << "Hello Cwordle!\n";
     std::string chosenWord = getNewWord();
@@ -50,13 +51,11 @@ int main() {
     std::string notInWord; // letters that are not in the word at all
     for (int b = 0; b < 6; b++) {
         std::string guessedWord = guessWord();
-        
         // Ends the program if guessed correctly
         if (guessedWord == chosenWord) {
             std::cout << "\nCorrect! The word was: " << chosenWord;
             return 0;
         }
-
         // Check letters in the correct spaces
         for (int i = 0; i < 5; i++) {
             // std::vector<char> l(g.begin(), g.end());
@@ -67,7 +66,6 @@ int main() {
                 correct.push_back(0x23);
          }
         }
- 
         // Check for existing letters in the wrong space but in the word
         // Set up this way in order to not give away the word
         for (int y = 0; y < 5; y++) {
@@ -87,13 +85,10 @@ int main() {
             }
         }
         wrongSpot = checkDupe(wrongSpot);
-      
         //return correct letters
         std::cout << "Letters in the correct spots: " << correct << "\n";
         correct.clear();
-      
         std::cout << "Letters in the word: " << wrongSpot << "\n";
-
         std::cout << "Letters not in the word at all: " << notInWord << "\n\n";
     }
   std::cout << "Out of guesses! The correct word was: " << chosenWord;
